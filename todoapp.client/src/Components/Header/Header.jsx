7d034c751cc3c76e7f43b847dcfd68683a87ee5c
Karan <Karan@sumedhasoftech.com>
2025-05-22 16:21:09 +0530

    add click event to menu instead of header

--- todoapp.client/src/Components/Header/Header.jsx
+++ todoapp.client/src/Components/Header/Header.jsx
@@ -6,9 +6,9 @@ const Header = () => {
     console.log(hideSidebar);
     return (
         <div className="header">
-            <div className="row d-flex justify-content-center text-left" onClick={() => setHideSidebar(!hideSidebar)}>
+            <div className="row d-flex justify-content-center text-left" >
                 <div className="col-1">
-                    <SquareMenu className="squaremenu-btn"  />
+                    <SquareMenu className="squaremenu-btn" onClick={() => setHideSidebar(!hideSidebar)} />
                 </div>
                 <div className="col-11 text-end">
                     <p> RAMLAKHAN'S TO DO APP </p>
